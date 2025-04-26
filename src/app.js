bcfee7a06596b0f445f074e808f99ce6cc4ec475
Solizra <167431784+Solizra@users.noreply.github.com>
2025-04-26 16:38:35 -0300

    "Avances finales APP"

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ import { horaActual, fechaCompleta } from './time.js';
 
 const app = express();
 
-
 app.get('/', (req, res) => { 
-    res.send('Bienvenido a mi servidor');
+    res.send('Bienvenido a mi servidor!');
 });
 
 app.get('/hora', (req, res) => {
     res.send(horaActual());
-  });
+});
 
 app.get('/fecha-completa', (req, res) => {
     res.send(fechaCompleta());
-  });
+});
+
+//EXTRA
+app.use((req, res) => {
+res.status(404).send('Datos incompletos <img src="https://http.cat/404" alt="404 Bad Request"/>');
+});
+
 
 export default app
